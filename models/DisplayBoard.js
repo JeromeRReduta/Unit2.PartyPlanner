adb5a0c218a2366cd841aa0adb18adc69de30dad
JRReduta <jrreduta@yahoo.com>
2025-06-14 01:56:17 -0700

    Finished model

--- models/DisplayBoard.js
+++ models/DisplayBoard.js
@@ -7,4 +7,12 @@ export default class DisplayBoard {
     this.items = [];
     this.selectedItem = null;
   }
+
+  async getItems(APICaller) {
+    this.items = await APICaller.getAll();
+  }
+
+  async getItem(APICaller, id) {
+    this.selectedItem = await APICaller.get(id);
+  }
 }
