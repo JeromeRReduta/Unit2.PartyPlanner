bcb321358b0528f70b139c08e44cff7fc3515683
JRReduta <jrreduta@yahoo.com>
2025-06-14 03:15:34 -0700

    Began controller

--- controllers/DisplayBoardController.js
+++ controllers/DisplayBoardController.js
@@ -1,1 +1,36 @@
-export default class DisplayBoardController {}
+export default class DisplayBoardController {
+    #board;
+
+    #view;
+
+    #reader;
+
+    constructor(board, view, reader) {
+        this.#board = board;
+        this.#view = view;
+        this.#reader = reader;
+    }
+
+    async init() {
+        // set up stuff.....
+
+        document.addEventListener("click", (event) => {
+            /* event => this.selectItem(id) */
+            /* then this.#view.render() */
+        });
+    }
+
+    async selectItem(id) {}
+
+    get board() {
+        return this.#board;
+    }
+
+    get view() {
+        return this.#view;
+    }
+
+    get reader() {
+        return this.#reader;
+    }
+}
